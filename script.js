3be9bd2034973c850918ed8b08ca36bbe41c4a52
Pesandu Rashodya <pesandurashodya@gmail.com>
2024-10-20 09:12:52 +0530

    responsive design

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const navContainer = document.querySelector('.nav-container');
 menuToggle.addEventListener('click', () => {
   navContainer.classList.toggle('active');
 });
+
+function loadHTML (filename, elementId) {
+  fetch(filename)
+  .then(response => response.text())
+  .then(data => document.getElementById(elementId).innerHTML = data);
+}
+
+window.onload = function(){
+  loadHTML('header.html', 'header');
+  loadHTML('footer.html', 'footer');
+}
